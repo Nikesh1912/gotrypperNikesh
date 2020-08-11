e521a0e39f2428d63817845df4491276ffb9ebf7
Nikesh1912 <nikeshsingh.1912@email.com>
2020-08-11 11:56:32 -0700

    admin service for guide

---
+++ technology/src/main/java/com/goTrypper/technology/admin/controller/AdminController.java
@@ -0,0 +1,21 @@
+/*
+package com.goTrypper.technology.admin.controller;
+
+import com.goTrypper.technology.domain.request.CouponRequest;
+import com.goTrypper.technology.domain.response.CouponAPIResponse;
+import com.goTrypper.technology.service.api.CouponService;
+
+
+@RestController
+@RequestMapping(value = "/v1")
+public class AdminController {
+    @Autowired
+    private CouponService couponService;
+
+    @PostMapping(value = "/validateCoupon", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public CouponAPIResponse searchCoupon(@RequestBody CouponRequest couponRequest)  {
+        return couponService.validateCoupon(couponRequest);
+    }
+}
+*/
+
